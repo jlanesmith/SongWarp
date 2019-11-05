560e3c2f212fe9b75e34896ff3c754de495e335f
Jonathan Lane-Smith <jlanesmith1@gmail.com>
2019-11-04 21:46:33 -0500

    Add commenting and make minor improvements

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
+/*
+	This file contains the source code for SongBack (patent pending)
+
+	Author: Jonathan Lane-Smith
+
+	Brief overview: Listening information is retrieved in packets of a week is contained in a large array named `songs`.
+	Brief description of steps involved:
+	1. Check whether the user is valid. If the user is valid, continue and if not, output results
+	2. Using the given start date and end date, calculate the start and end dates for each week in both the "previous" and "current" sections. 
+	3. For each week in the "previous" section, make an http call to get every song listened to that week. These songs are added to the `songs` array.
+	4. Repeat step 3, but with each week in the "current" section.
+	5. Using the previousLimit and currentLimit, determine which songs pass the requirements and will be included in the results.
+	6. Sort the resulting songs and output them.
+*/
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const request = require('request');
@@ -15,20 +30,21 @@ app.get('/', function (req, res) {
 var responder;
 
 var username;
-var startDate;
-var startDateText;
-var endDate;
-var endDateText;
-var previousLimit;
-var currentLimit;
+var startDate; // Start date, determined from startDateText
+var startDateText; // Text received through UI
+var endDate; // End date, determined from endDateText
+var endDateText; // Text received through UI
+var previousLimit; // User must have listened to songs at least this many times before the start date
+var currentLimit; // User must have listened to songs no more than this many times since the end date
 
-var previousDates = [];
-var currentDates = [];
-var songs = [];
-var results = [];
+var previousDates = []; // 2D array that contains the start date and end date for each week included in the "previous" section
+var currentDates = []; // 2D array that contains the start date and end date for each week included in the "current" section
+var songs = []; // Array of all songs. Each song is an object containing an artist, name, playcount, url and also a "previousTotal" and "currentTotal"
+var results = []; // Array of songs that pass the parameters and will be included in the output
 
 var errorMessage = null;
 
+// Check to see if the username is valid. If it is, continue with calculations and if not, output results with an error message
 function checkUser(callback) {
 	http.get('http://ws.audioscrobbler.com/2.0/?method=user.getInfo&user='+ username +
 	'&api_key=67d2877611ab7f461bda654cb05b53ae&format=json', (resp) => {
@@ -51,8 +67,9 @@ function checkUser(callback) {
 	});
 }
 
+// Waits for the user to click submit and then starts error checking and doing calculations
 function getPost() {
-  app.post('/', function (req, res) {
+  	app.post('/', function (req, res) {
 		responder = res;
 		username = req.body.username;
 		startDateText = req.body.startDate;
@@ -69,65 +86,71 @@ function getPost() {
 
 getPost();
 
-app.listen(process.env.PORT);
+app.listen(process.env.PORT); // Change to 3000 when debugging
 
-function addSong(songs, newSong, previousFlag) {
+// Takes a song and adds it to the songs array
+function addSong(newSong, previousFlag) {
 	for (var i = 0; i < songs.length; i++) {
+
+		// If the song already exists in the array, update the playcount
 		if ( (songs[i].name == newSong.name) && (songs[i].artist['#text'] == newSong.artist['#text']) ) {
 			if (previousFlag) {
 				songs[i].previousTotal += parseInt(newSong.playcount, 10);
 			} else {
 				songs[i].currentTotal += parseInt(newSong.playcount, 10);
 			}
-			return songs;
+			return;
 		}
 	}
+	// If the song doesn't already exist in the array, add it to the array
 	songs.push(newSong);
-	return songs;
 }
 
-function sortBySongPreviousTotal(songs) {
-	return songs.sort(function(a,b){return b.previousTotal - a.previousTotal});
-}
-
+// Converts date to time stamp
 function dateToTS(dateText) {
 	var date = new Date(dateText);
 	return Math.round(date.getTime()/1000);
 }
 
+// Create a 2D array that contains the start and end dates of weeks for either the "previous" or "current" section
 function getDates(startDate, endDate, callback) {
 
-	var dates = [];
+	var dates = []; // This array will be sent into the callback and be assigned to either previousDates or currentDates
+
+	// Gets list of available charts for this user, expressed as date ranges
 	http.get('http://ws.audioscrobbler.com/2.0/?method=user.getweeklychartlist&user='+ username +
 		'&api_key=67d2877611ab7f461bda654cb05b53ae&format=json', (resp) => {
-	  let data = '';
-	  resp.on('data', (chunk) => {
-	    data += chunk;
-	  });
-	  resp.on('end', () => {
-		  let list = JSON.parse(data).weeklychartlist;
-	  	for (var i = 0; i < list.chart.length; i++) {
-	  		if ((list.chart[i].from > startDate) && (list.chart[i].to < endDate)) {
-	  			dates.push(new Array(list.chart[i].from, list.chart[i].to));
-	  		}
-	  	}
-	  	callback(dates);
-	  }); 
+	  	let data = '';
+	  	resp.on('data', (chunk) => {
+	   	 data += chunk;
+		});
+		resp.on('end', () => {
+		  	let list = JSON.parse(data).weeklychartlist;
+			for (var i = 0; i < list.chart.length; i++) {
+				if ((list.chart[i].from > startDate) && (list.chart[i].to < endDate)) {
+					dates.push(new Array(list.chart[i].from, list.chart[i].to));
+				}
+			}
+			callback(dates);
+		}); 
 	}).on("error", (err) => {
-	  console.log("Error: " + err.message);
-	  errorMessage = "Error: " + err.message;
+		console.log("Error: " + err.message);
+		errorMessage = "Error: " + err.message;
 	});
 }
 
-function getSongs(dateNumber, previousFlag, callback) {
+// Get songs for a specific week
+// dateNumber represents which week it is, with 0 being the earliest week and each following week being incremented by 1
+// isPrevious represents whether this week is included in the "previous" section or "current" section
+function getSongs(dateNumber, isPrevious, callback) {
 
-	var start;
-	var end;
-	let progress = dateNumber + 1 + (previousFlag ? 0 : previousDates.length);
-	var percentDone = progress / (previousDates.length + currentDates.length);
+	var start; // Date of beginning of week
+	var end; // Date of end of week
+	let progress = dateNumber + 1 + (isPrevious ? 0 : previousDates.length);
 	console.log("Progress: " + progress + " of " + (previousDates.length + currentDates.length) );
 
-	if (previousFlag) {
+	// Find beginning and end dates of the week
+	if (isPrevious) {
 		start = previousDates[dateNumber][0];
 		end = previousDates[dateNumber][1];
 	} else {
@@ -142,23 +165,33 @@ function getSongs(dateNumber, previousFlag, callback) {
 	    data += chunk;
 	  });
 	  resp.on('end', () => {
-	  	var chart = JSON.parse(data).weeklytrackchart;
+
+		if (resp.statusCode != 200) {
+			console.log("Response code while getting weekly song data:");
+			console.log(resp.statusCode);
+		}
+
+		// Get list of songs in the week
+		var chart = JSON.parse(data).weeklytrackchart;
+		// Add each song to the big array of all the songs 
 	  	for (var i = 0; i < chart.track.length; i++) {
 	  		var song = chart.track[i];
 	  		song["previousTotal"] = 0;
 	  		song["currentTotal"] = 0;
-	  		songs = addSong(songs, song, previousFlag);
-	  	}
+	  		addSong(song, isPrevious);
+		}
+		
+		// Determine whether to go to the next week, or whether this is the last week of the section
 	  	var datesToCompare;
-	  	if (previousFlag) {
+	  	if (isPrevious) {
 	  		datesToCompare = previousDates;
 	  	} else {
 	  		datesToCompare = currentDates;
 	  	}
 	  	if (dateNumber + 1 == datesToCompare.length) {
-	  		callback();
+	  		callback(); // Finished either "previous" or "current" section
 	  	} else {
-	  		getSongs(dateNumber + 1, previousFlag, callback);
+	  		getSongs(dateNumber + 1, isPrevious, callback); // Get songs for the next week
 	  	}
 
 	  }); 
@@ -168,6 +201,7 @@ function getSongs(dateNumber, previousFlag, callback) {
 	});
 }
 
+// This is the main calculation function and shows the general path that the program goes through
 function calculateDates() {
 	
 	getDates(startDate, endDate, function(previousDates1){
@@ -175,9 +209,9 @@ function calculateDates() {
 			previousDates = previousDates1;
 			currentDates = currentDates1;
 			if (previousDates.length > 0) {
-				getSongs(0, true, function() {
+				getSongs(0, true, function() { // Get songs contained in the "previous" section
 					if (currentDates.length > 0) {
-						getSongs(0, false, function() {
+						getSongs(0, false, function() { // Get songs contained in the "current" section
 							outputResults();
 						})
 					} else {
@@ -185,29 +219,43 @@ function calculateDates() {
 					}
 				});
 			} else {
-				outputResults();
+				outputResults(); // If there are no weeks contained in the previousDates array, there is no valid information
 			}
 		});
 	});
 }
 
+// Sort results by the total number of listens during the "previous" section
+function sortResultsByPreviousTotal() {
+	return results.sort(function(a,b){return b.previousTotal - a.previousTotal});
+}
+
+// Output results once either the username is found to be invalid, or the calculations have been complete
 function outputResults() {
 
+	// Determine which songs pass the requirements and count towards the results
 	for (var i = 0; i < songs.length; i++) {
 		if ( (songs[i].previousTotal >= previousLimit) && (songs[i].currentTotal <= currentLimit)) {
 			songs[i]["artistText"] = songs[i].artist['#text'];
 			results.push(songs[i]);
 		}
 	}
-	results = sortBySongPreviousTotal(results);
-	var resultText = [];
+	sortResultsByPreviousTotal(); // Sort results
+
+	var resultText = []; // Array that contains all of the information for the results
 	var info = "Parameters given: " + username + ", " + startDateText + ", " + endDateText + ", " + previousLimit + ", " + currentLimit;
 	console.log(info);
 	console.log("Results: " + results.length);
-	for (var i = 0; i < results.length; i++) {	
+	for (var i = 0; i < results.length; i++) {
 		resultText[i] = new Array(results[i].name, results[i].artist['#text'], results[i].previousTotal, results[i].currentTotal);
 	}
 	responder.render('index', {info: info, results: resultText, error: errorMessage});
+
+	// Reset variables and prepare to the run the program again
 	errorMessage = null;
-	getPost();
+	songs = [];
+	results = [];
+	previousDates = [];
+	currentDates = [];
+	getPost(); // Prepare the program to receive new parameters and make calculations again
 }
